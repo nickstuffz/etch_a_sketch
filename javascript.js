c022a717cd5afc03dc4f54564bf1dd9c07ae076b
nickstuffz <nicholasng62@gmail.com>
2023-04-29 13:53:44 -0700

    Create hover effect on grid

--- javascript.js
+++ javascript.js
@@ -2,13 +2,27 @@
 
 const container = document.querySelector('#grid-container');
 for (i=0; i<16; i++) {
-    const row = document.createElement('div');
-    container.appendChild(row);
+    const column = document.createElement('div');
+    container.appendChild(column);
     for (j=0; j<16; j++) {
-        const grid = document.createElement('div');
-        grid.style.border = '1px solid black';
-        grid.style.width = '40px';
-        grid.style.height = '40px';
-        row.appendChild(grid);
+        const square = document.createElement('div');
+        square.style.border = '1px solid black';
+        square.style.width = '40px';
+        square.style.height = '40px';
+        square.setAttribute('class', 'square');
+        column.appendChild(square);
     };
 };
+// set up hover effect
+// for each div, add hover event listener, changes color on hover
+// figure out how to revert back afterwards
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'pink';
+    });
+    // square.addEventListener('mouseout', () => {
+    //     square.style.backgroundColor = 'white';
+    // });
+});
