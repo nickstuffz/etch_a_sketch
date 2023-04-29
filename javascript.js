a7e111b63b3fd68d4428cb27cc505635320236b2
nickstuffz <nicholasng62@gmail.com>
2023-04-29 13:30:27 -0700

    Create grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 // script it
+
+const container = document.querySelector('#grid-container');
+for (i=0; i<16; i++) {
+    const row = document.createElement('div');
+    container.appendChild(row);
+    for (j=0; j<16; j++) {
+        const grid = document.createElement('div');
+        grid.style.border = '1px solid black';
+        grid.style.width = '40px';
+        grid.style.height = '40px';
+        row.appendChild(grid);
+    };
+};
